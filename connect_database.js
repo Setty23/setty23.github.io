68d2631f13ac1fb894469fb0ebb378b912ccc52d
Setty23 <125440661+Setty23@users.noreply.github.com>
2024-05-14 23:01:50 +0300

    Create connect_database.js

---
+++ connect_database.js
@@ -0,0 +1,8 @@
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database');
+});
